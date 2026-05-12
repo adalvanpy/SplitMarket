a49673a764b18a96f2541a4924945ff68e1f90ba
Maíra Ramos <ramosmaira369@gmail.com>
2026-05-12 08:54:00 -0300

    Configurando a  navegação inicial do aplicativo (atualizando o main.dart com a estrutura base do app

--- splitmarket/lib/main.dart
+++ splitmarket/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'views/login/login_page.dart';
+import 'views/login/register_page.dart';
+import 'views/home/home_page.dart';
+import 'views/group/group_page.dart';
+import 'views/expense/expense_page.dart';
+import 'views/expense/add_expense_page.dart';
+import 'views/summary/summary_page.dart';
+import 'views/settings/settings_page.dart';
 
 void main() {
   runApp(const SplitMarketApp());
@@ -13,13 +20,27 @@ class SplitMarketApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       title: 'SplitMarket',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.green,
         ),
       ),
-      home: const LoginPage(),
+
+      initialRoute: '/login',
+
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/home': (context) => const HomePage(),
+        '/group': (context) => const GroupPage(),
+        '/expenses': (context) => const ExpensePage(),
+        '/add-expense': (context) => const AddExpensePage(),
+        '/sumary': (context) => const SummaryPage(),
+        '/settings': (context) => const SettingsPage(),
+      },
     );
   }
 }
